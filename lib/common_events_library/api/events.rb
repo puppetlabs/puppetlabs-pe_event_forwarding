fd29935d62f462a6cde297eb82e59e8ec0b93380
gregleesparks@gmail.com <gregleesparks@gmail.com>
2020-11-18 12:06:05 -0800

    (PIE-419) Convert api modules to classes
    This changes converts the api modules to classes in order to use an init
    method to create the http object which can be reused.

--- lib/common_events_library/api/events.rb
+++ lib/common_events_library/api/events.rb
@@ -1,11 +1,15 @@
 require_relative '../util/pe_http'
 
 # module Events This contains the API specific code for the events API
-module Events
-  def get_all_events(pe_console, username, password, service = 'classifier', offset = 0, limit = 0, ssl_verify: true)
-    http = PeHttp.new(pe_console, port: 4433, username: username, password: password, ssl_verify: ssl_verify)
-    uri = PeHttp.make_params("activity-api/v1/events?service_id=#{service}", limit, offset)
-    http.pe_get_request(uri)
+class Events
+  attr_accessor :pe_client
+
+  def initialize(pe_console, username, password, ssl_verify: true)
+    @pe_client = PeHttp.new(pe_console, port: 4433, username: username, password: password, ssl_verify: ssl_verify)
   end
-  module_function :get_all_events
+
+  def get_all_events(service: 'classifier', offset: 0, limit: 0)
+    uri = PeHttp.make_params("activity-api/v1/events?service_id=#{service}", limit, offset)
+    pe_client.pe_get_request(uri)
+  end
 end
