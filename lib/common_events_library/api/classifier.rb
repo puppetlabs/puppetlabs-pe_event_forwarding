993662344a0017560a377280df947fce017f95d8
gregleesparks@gmail.com <gregleesparks@gmail.com>
2020-11-25 08:42:46 -0800

    (MAINT) Fix pagination.
    Previously only the offset would be included if both a limit and offset
    were specified. This adds a few separate method including one to create
    a param string from a hash of params, one the create a hash of limit and
    offset params, and one to combine the two into a convinience method.

--- lib/common_events_library/api/classifier.rb
+++ lib/common_events_library/api/classifier.rb
@@ -14,7 +14,7 @@ class Classifier
   # pagination information, and possibly auth information for third party
   # services such as Splunk.
   def get_node_parameters(node, offset = 0, limit = 0)
-    uri = PeHttp.make_params("classifier-api/v1/classified/nodes/#{node}", limit, offset)
+    uri = PeHttp.make_pagination_params("classifier-api/v1/classified/nodes/#{node}", limit, offset)
     response = pe_client.pe_get_request(uri)
     raise "Failed to retreive node parameters: #{response.code}, #{response.message}" if response.code.to_i > 200
     JSON.parse(response.body)['parameters']
