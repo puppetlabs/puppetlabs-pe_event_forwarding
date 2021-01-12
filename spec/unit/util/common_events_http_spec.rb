e8960680918f14dfdf1944630050099d792e9e75
Greg Hardy <greg.hardy@puppetlabs.com>
2021-01-12 12:16:40 +0000

    maint

--- spec/unit/util/common_events_http_spec.rb
+++ spec/unit/util/common_events_http_spec.rb
@@ -10,9 +10,9 @@ describe 'Common Events Http' do
   end
 
   it 'makes pagination params correctly' do
-    expect(CommonEventsHttp.make_pagination_params('orchestrator/v1/jobs', 5, 2)).to eq('orchestrator/v1/jobs?limit=5&offset=2')
-    expect(CommonEventsHttp.make_pagination_params('orchestrator/v1/jobs', 0, 2)).to eq('orchestrator/v1/jobs?offset=2')
-    expect(CommonEventsHttp.make_pagination_params('orchestrator/v1/jobs', 5, 0)).to eq('orchestrator/v1/jobs?limit=5')
+    expect(CommonEventsHttp.make_pagination_params('orchestrator/v1/jobs', 5, 2)).to eq('orchestrator/v1/jobs&limit=5&offset=2')
+    expect(CommonEventsHttp.make_pagination_params('orchestrator/v1/jobs', 0, 2)).to eq('orchestrator/v1/jobs&offset=2')
+    expect(CommonEventsHttp.make_pagination_params('orchestrator/v1/jobs', 5, 0)).to eq('orchestrator/v1/jobs&limit=5')
     expect(CommonEventsHttp.make_pagination_params('orchestrator/v1/jobs', 0, 0)).to eq('orchestrator/v1/jobs')
   end
 
