3a7d7aa095ee2e061a1fc9dd3446b290cd29aadf
Bill Hurt <hurt.bill@gmail.com>
2021-07-26 09:57:54 -0700

    (PIE-761) Implement Search for Processors
    This change adds a class that knows how to search a specified directory
    at only the first level looking for files, but not search recursively
    through directories. Those directories may contain support library files
    for the processors, not processors.
    
    This class can then be created from a file path and it knows how to
    invoke a processor and capture its output streams and exit code.
    
    That data can then be accessed for logging purposes.

--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -1,5 +1,13 @@
+require 'spec_helper'
+
 RSpec.configure do |c|
   c.mock_with :rspec
 end
 
 FULL_MODULE_PATH = "#{Dir.pwd}/spec/fixtures/modules".freeze
+
+def procs_paths
+  base_path = "#{Dir.pwd}/spec/support/"
+  Dir.children('./spec/support').map { |p| "#{base_path}#{p}" } +
+    Dir.children('spec/support/acceptance').map { |p| "#{base_path}#{p}" }
+end
