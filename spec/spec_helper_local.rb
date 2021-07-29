665499038b426f231f88d94d8ff967852228df35
Bill Hurt <hurt.bill@gmail.com>
2021-07-29 10:57:05 -0700

    (MAINT) Improve Index Class
    Remove redundant file reads
    Add a count function
    Improve save function

--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -1,5 +1,3 @@
-require 'spec_helper'
-
 RSpec.configure do |c|
   c.mock_with :rspec
 end
@@ -10,4 +8,19 @@ def procs_paths
   base_path = "#{Dir.pwd}/spec/support/"
   Dir.children('./spec/support').map { |p| "#{base_path}#{p}" } +
     Dir.children('spec/support/acceptance').map { |p| "#{base_path}#{p}" }
+end
+
+def index_data(**custom_count)
+  template = {
+    classifier:     0,
+    rbac:           0,
+    'pe-console':   0,
+    'code-manager': 0,
+    orchestrator:   0,
+  }
+  template.merge(custom_count)
+end
+
+def index_yaml(**custom_count)
+  index_data(custom_count).to_yaml
 end
